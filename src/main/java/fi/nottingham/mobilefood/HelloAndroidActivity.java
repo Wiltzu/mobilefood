a43a34c771ad292f3370dd895a25b8f771161570
Wille2 <villeahti@hotmail.com>
2013-11-01 17:45:16 +0200

    now it should work?

--- src/main/java/fi/nottingham/mobilefood/HelloAndroidActivity.java
+++ src/main/java/fi/nottingham/mobilefood/HelloAndroidActivity.java
@@ -2,7 +2,6 @@ package fi.nottingham.mobilefood;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 
 public class HelloAndroidActivity extends Activity {
