430488b5c85f0913d40f0d38c9fe069d3c6e58cd
Wille2 <villeahti@hotmail.com>
2013-11-03 21:25:54 +0200

    Hello Roboguice: just a little try to do something with it

--- src/main/java/fi/nottingham/mobilefood/HelloAndroidActivity.java
+++ src/main/java/fi/nottingham/mobilefood/HelloAndroidActivity.java
@@ -1,29 +1,40 @@
 package fi.nottingham.mobilefood;
 
-import android.app.Activity;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
-public class HelloAndroidActivity extends Activity {
+@ContentView(R.layout.activity_main)
+public class HelloAndroidActivity extends RoboActivity {
+	
+	@InjectView(R.id.textfield_hello)
+	TextView text;
+	
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		text.setText("Hello world changed!");
+		text.setBackgroundColor(Color.BLUE);
+	}
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(fi.nottingham.mobilefood.R.menu.main, menu);
-	return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(fi.nottingham.mobilefood.R.menu.main, menu);
+		return true;
+	}
 
 }
-
