ff1a9a089bf1b61503a4a7751a1f2848300046e5
Wille2 <villeahti@hotmail.com>
2013-11-07 21:07:17 +0200

    Now testing with Robolectric is possible

--- src/test/java/fi/nottingham/mobilefood/HelloAndroidActivityTest.java
+++ src/test/java/fi/nottingham/mobilefood/HelloAndroidActivityTest.java
@@ -1,21 +1,33 @@
 package fi.nottingham.mobilefood;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 
 import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
 import android.widget.TextView;
-import fi.nottingham.mobilefood.util.InjectedTestRunner;
 
-@RunWith(InjectedTestRunner.class)
+import com.google.inject.Inject;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+import fi.nottingham.mobilefood.util.TestGuiceModule;
+
+/**
+ * This is very stupid test expirement. 
+ * 
+ * @author Ville
+ *
+ */
+@RunWith(RobolectricTestRunner.class)
 public class HelloAndroidActivityTest {
 
-	//@Inject
-	//HelloAndroidActivity activityInTest;
+	@Inject
+	HelloAndroidActivity activityInTest;
 	@InjectResource(R.string.hello_world)
 	String caption;
 	@InjectView(R.id.textfield_hello)
@@ -23,11 +35,19 @@ public class HelloAndroidActivityTest {
 	
 	@Before
 	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	    TestGuiceModule module = new TestGuiceModule();
+	    TestGuiceModule.setUp(this, module);
 	}
 
 	@Test
 	public void test() {
-		assertFalse(textView.getText() != caption);
+		assertNotNull("caption can't be null", caption);
+		//assertNotNull(textView); injecting views don't work for an unknown reason
+		activityInTest.onCreate(null);
+		textView = (TextView) activityInTest.findViewById(R.id.textfield_hello);
+		String actualText = (String) textView.getText();
+		assertTrue(actualText != caption);
 	}
 
 }
---
+++ src/test/java/fi/nottingham/mobilefood/util/TestGuiceModule.java
@@ -0,0 +1,67 @@
+package fi.nottingham.mobilefood.util;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import roboguice.RoboGuice;
+import roboguice.config.DefaultRoboModule;
+import roboguice.inject.ContextSingleton;
+import roboguice.inject.RoboInjector;
+import android.app.Activity;
+import android.app.Application;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.util.Modules;
+import com.xtremelabs.robolectric.Robolectric;
+
+public class TestGuiceModule extends AbstractModule {
+
+	private HashMap<Class<?>, Object> bindings;
+
+	public TestGuiceModule() {
+		bindings = new HashMap<Class<?>, Object>();
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	protected void configure() {
+		bind(Activity.class).toProvider(DummyActivityProvider.class).in(ContextSingleton.class);
+		Set<Entry<Class<?>, Object>> entries = bindings.entrySet();
+		for (Entry<Class<?>, Object> entry : entries) {
+			bind((Class<Object>) entry.getKey()).toInstance(entry.getValue());
+		}
+	}
+
+	public void addBinding(Class<?> type, Object object) {
+		bindings.put(type, object);
+	}
+
+	public static void setUp(Object testObject, TestGuiceModule module) {
+		Module roboGuiceModule = RoboGuice
+				.newDefaultRoboModule(Robolectric.application);
+		Module testModule = Modules.override(roboGuiceModule).with(module);
+		RoboGuice.setBaseApplicationInjector(Robolectric.application,
+				RoboGuice.DEFAULT_STAGE, testModule);
+		RoboInjector injector = RoboGuice.getInjector(Robolectric.application);
+		injector.injectMembers(testObject);
+	}
+
+	public static void tearDown() {
+		RoboGuice.util.reset();
+		Application app = Robolectric.application;
+		DefaultRoboModule defaultModule = RoboGuice.newDefaultRoboModule(app);
+		RoboGuice.setBaseApplicationInjector(app, RoboGuice.DEFAULT_STAGE,
+				defaultModule);
+	}
+}
+
+class DummyActivityProvider implements Provider<Activity> {
+
+	public Activity get() {
+		return new Activity();
+	}
+
+}
