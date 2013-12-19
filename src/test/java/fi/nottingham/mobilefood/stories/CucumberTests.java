839a8771cc55f541fb1f23351b7f30d090acb71e
Wille2 <villeahti@hotmail.com>
2013-12-19 19:47:29 +0200

    migrated project to be compatible with cucumber

---
+++ src/test/java/fi/nottingham/mobilefood/stories/CucumberTests.java
@@ -0,0 +1,12 @@
+package fi.nottingham.mobilefood.stories;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+//@CucumberOptions(format = "pretty")
+public class CucumberTests {
+
+
+}
